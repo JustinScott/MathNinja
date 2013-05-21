9bdf5c64a140b0fa754fb496f043f19f44c4ad03
Justin Scott <jsco976@gmail.com>
2013-05-21 04:51:28 -0500

    fixed up the options activity: removed title, added animations, and a first run check in onStart

--- src/com/example/testapp/OptionsActivity.java
+++ src/com/example/testapp/OptionsActivity.java
@@ -8,10 +8,15 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class OptionsActivity extends Activity {
+
+    public boolean FIRSTRUN = true;
 
 	public final static String OPERATION = "com.example.testapp.OPERATION";
 	public final static String LEVEL = "com.example.testapp.LEVEL";
@@ -23,22 +28,58 @@ public class OptionsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
         
-        TextView textview = (TextView) findViewById(R.id.textView1);
-        Typeface font = Typeface.createFromAsset(getAssets(), "DK Crayon Crumble.ttf");
-        textview.setTypeface(font);
+        //TextView textview = (TextView) findViewById(R.id.textView1);
+        //Typeface font = Typeface.createFromAsset(getAssets(), "DK Crayon Crumble.ttf");
+        //textview.setTypeface(font);
     }
     
     @Override
-    public void onResume() {
-    	super.onResume();
-   		mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.smartie);
-    	mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
-            }
-        });
-		mediaPlayer.start();
+    public void onStart() {
+    	super.onStart();
+
+        if(FIRSTRUN) {
+            mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.smartie);
+            mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+            mediaPlayer.start();
+
+            Button button1 = (Button) findViewById(R.id.button1);
+            Button button2 = (Button) findViewById(R.id.button2);
+            Button button3 = (Button) findViewById(R.id.button3);
+            Button button4 = (Button) findViewById(R.id.button4);
+
+            TranslateAnimation lrAnimation = new TranslateAnimation(-500f, 0f, 0f, 0f);
+            TranslateAnimation rlAnimation = new TranslateAnimation(500f, 0f, 0f, 0f);
+            AlphaAnimation aAnimation = new AlphaAnimation(0.0f, 1.0f);
+
+            AnimationSet animationSet1 = new AnimationSet(true);
+            animationSet1.setDuration(3000);
+            animationSet1.addAnimation(aAnimation);
+            animationSet1.addAnimation(lrAnimation);
+
+            AnimationSet animationSet2 = new AnimationSet(false);
+            animationSet2.setDuration(3000);
+            animationSet2.addAnimation(aAnimation);
+            animationSet2.addAnimation(rlAnimation);
+
+            button1.setAnimation(animationSet1);
+            button1.setVisibility(View.VISIBLE);
+
+            button2.setAnimation(animationSet2);
+            button2.setVisibility(View.VISIBLE);
+
+            button3.setAnimation(animationSet1);
+            button3.setVisibility(View.VISIBLE);
+
+            button4.setAnimation(animationSet2);
+            button4.setVisibility(View.VISIBLE);
+
+            FIRSTRUN = false;
+        }
     }
     
     @Override
